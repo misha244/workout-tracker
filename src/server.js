0e3f8efa914e8d4d3edb62a4d67fa638d1bab2ce
Misha <73886737+misha244@users.noreply.github.com>
2021-07-18 20:57:14 +0100

    edit paths

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const routes = require("./routes");
+const routes = require("./routes/index");
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,12 +20,12 @@ const MONGOOSE_OPTIONS = {
 mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
 
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./src/public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("./src/public"));
 
 // routes
 app.use(routes);
