cdef243727abac0979af587b72de547b92026e9b
Misha <73886737+misha244@users.noreply.github.com>
2021-07-18 18:47:07 +0100

    delete config.js, move to server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
-const DB_NAME = "workout";
+
+const DB_NAME = process.env.DB_NAME || "workout";
+
+const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
+
+const MONGOOSE_OPTIONS = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
 
 // routes
 app.use(routes);
