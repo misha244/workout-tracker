3a6f0afe81e1971fbd6567e080acc6c75088dc7d
Misha <73886737+misha244@users.noreply.github.com>
2021-07-18 20:09:42 +0100

    wrap fns in try/catch blocks

--- src/controllers/api/workout.js
+++ src/controllers/api/workout.js
@@ -1,42 +1,52 @@
 const { Workout } = require("../../models");
 
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({});
-  res.json(workouts);
+  try {
+    const workouts = await Workout.find({});
+    res.json(workouts);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const continueWorkout = async (req, res) => {
   const { id } = req.params;
+  try {
+    const data = await Workout.findByIdAndUpdate(id, {
+      $push: {
+        exercises: req.body,
+      },
+    });
 
-  const data = await Workout.findByIdAndUpdate(id, {
-    $push: {
-      exercises: req.body,
-    },
-  });
-
-  res.json(data);
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const createWorkout = async (req, res) => {
   const { name, type, duration, weight, reps, sets, distance } = req.body;
+  try {
+    const workout = {
+      day: new Date(new Date().setDate(new Date().getDate())),
+      exercises: [
+        {
+          name,
+          type,
+          duration,
+          weight,
+          reps,
+          sets,
+          distance,
+        },
+      ],
+    };
 
-  const workout = {
-    day: new Date(new Date().setDate(new Date().getDate())),
-    exercises: [
-      {
-        name,
-        type,
-        duration,
-        weight,
-        reps,
-        sets,
-        distance,
-      },
-    ],
-  };
-
-  const data = await Workout.create(workout);
-  res.json(data);
+    const data = await Workout.create(workout);
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
