ac8dc75c8731abb2a817624ff597b92ad3918205
Misha <73886737+misha244@users.noreply.github.com>
2021-07-18 19:34:59 +0100

    wip

--- src/controllers/api/workout.js
+++ src/controllers/api/workout.js
@@ -1,17 +1,17 @@
 const { Workout } = require("../../models");
 
-const getWorkouts = async () => {
+const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({});
   res.json(workouts);
 };
 
-const continueWorkout = async () => {};
+const continueWorkout = async (req, res) => {};
 
-const createWorkout = async () => {
+const createWorkout = async (req, res) => {
   const { name, type, duration, weight, reps, sets, distance } = req.body;
 
   const workout = {
-    day: new Date(),
+    day: new Date(new Date().setDate(new Date().getDate())),
     exercises: [
       {
         name,
@@ -26,7 +26,7 @@ const createWorkout = async () => {
   };
 
   const data = await Workout.create(workout);
-  res.json(workout);
+  res.json(data);
 };
 
 module.exports = {
--- src/routes/api/workouts.js
+++ src/routes/api/workouts.js
@@ -1,5 +1,14 @@
 const { Router } = require("express");
 
+const {
+  getWorkouts,
+  continueWorkout,
+  createWorkout,
+} = require("../../controllers/api/workout");
+
 const router = Router();
+
+router.get("/", getWorkouts);
+router.post("/", createWorkout);
 
 module.exports = router;
