b906693842c8761b3fef604b7167d51a53535773
Misha <73886737+misha244@users.noreply.github.com>
2021-07-20 16:02:52 +0100

    add workoutsinrange fn

--- src/controllers/api/workout.js
+++ src/controllers/api/workout.js
@@ -55,7 +55,27 @@ const createWorkout = async (req, res) => {
   }
 };
 
-const getWorkoutsInRange = async (req, res) => {};
+const getWorkoutsInRange = async (req, res) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+      .sort({
+        _id: -1,
+      })
+      .limit(7);
+
+    res.json(workouts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   getWorkouts,
