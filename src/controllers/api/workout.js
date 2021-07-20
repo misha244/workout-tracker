657d038ffd45eba77e9183ffc54c7fa7aa63e593
Misha <73886737+misha244@users.noreply.github.com>
2021-07-20 16:00:18 +0100

    set up workout range fn

--- src/controllers/api/workout.js
+++ src/controllers/api/workout.js
@@ -55,11 +55,11 @@ const createWorkout = async (req, res) => {
   }
 };
 
-//const addExercise = async (req, res) => {};
+const getWorkoutsInRange = async (req, res) => {};
 
 module.exports = {
   getWorkouts,
   continueWorkout,
   createWorkout,
-  //addExercise,
+  getWorkoutsInRange,
 };
--- src/routes/api/workouts.js
+++ src/routes/api/workouts.js
@@ -4,6 +4,7 @@ const {
   getWorkouts,
   continueWorkout,
   createWorkout,
+  getWorkoutsInRange,
 } = require("../../controllers/api/workout");
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/", getWorkouts);
 router.post("/", createWorkout);
 router.put("/:id", continueWorkout);
+router.get("/range", getWorkoutsInRange);
 
 module.exports = router;
